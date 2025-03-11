a0fbc6e12a952f3239d154dca7fa1fca328391b1
gigi <gigicaabreu2078@gmail.com>
2025-03-11 17:46:44 -0300

    DecolaTech

---
+++ src/test/java/me/dio/decolatech_2025/Decolatech2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.decolatech_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Decolatech2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
